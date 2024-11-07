2a44655ae9f6d740f91000ea370ef70fdabb9eee
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-11-07 20:50:42 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,15 @@ import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 export default async function Home() {
-  const {userId} = await auth()
+  const { userId } = await auth();
 
-  if(!userId){
-    redirect("/login")
-}
+  if (!userId) {
+    redirect("/login");
+  }
 
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="flex h-full items-center justify-center">
       <UserButton showName />
     </div>
   );
