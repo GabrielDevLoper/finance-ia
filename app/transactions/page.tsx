6e42b39c6bd2b217000bd7d7593bbbcbd011fae5
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-11-12 07:04:20 -0300

    feat: config setup prisma and page transaction init

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,13 @@
-const Transaction = () => {
-    return <h1>transaction</h1>
-}
+// import { db } from "../_lib/prisma";
+
+const Transaction = async () => {
+  // const transactions = await db.transaction.findMany({})
+
+  return (
+    <div>
+      <div className="flex w-full items-center justify-between"></div>
+    </div>
+  );
+};
 
 export default Transaction;
