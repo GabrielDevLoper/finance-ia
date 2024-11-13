7d329800963912cd3ff8a33fbdb0a7853d56883d
Gabriel Barreto <gabriel.devloper@gmail.com>
2024-11-13 00:12:33 -0300

    feat: create navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import { UserButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
+import Navbar from "./_components/navbar";
 
 export default async function Home() {
-  const { userId } = await auth();
+  // const { userId } = await auth();
 
-  if (!userId) {
-    redirect("/login");
-  }
+  // if (!userId) {
+  //   redirect("/login");
+  // }
 
   return (
-    <div className="flex h-full items-center justify-center">
-      <UserButton showName />
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
